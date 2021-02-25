7a4f7198c63db62bc134ef77582419753631ad7c
Raul <raul.cardoso@outlook.com.br>
2021-02-24 23:18:52 -0300

    .

---
+++ src/main/java/br/edu/utfpr/pb/atividadeAula1/AtividadeAula1Application.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.atividadeAula1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeAula1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeAula1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/pb/atividadeAula1/AtividadeAula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.atividadeAula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeAula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
